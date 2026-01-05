86b22122f42e23038226399add6687f132fdb8e4
JoojNicolas <nicolasflaviagabriel@gmail.com>
2026-01-05 17:57:54 -0300

    FaltaThree.js

--- src/js/main.js
+++ src/js/main.js
@@ -16,8 +16,36 @@ function load(){
     console.log(nota.value) 
 }
 
+importar.addEventListener("change", () =>{
+    let readFile = new FileReader()
+
+    if(importar.files[0]){
+        console.log('Enviando arquivo')
+
+        readFile.readAsText(importar.files[0]);
+
+        readFile.onload = function(){
+            nota.value = readFile.result;
+        }
+    }else{
+        console.log('Nenhum arquivo foi enviado!')
+    }
+
+})
+
 carregar.addEventListener("click", () =>{
     load();
 })
 
 load();
+
+exportar.addEventListener("click", () => {
+    const blob = new Blob([nota.value], {type: exportar.value});
+    const fileUrl = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+
+    link.download = "Omnia";
+    link.href = fileUrl;
+    link.click();
+})
+
